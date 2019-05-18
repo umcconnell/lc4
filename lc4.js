a7a73680b82251284d13d141dadfef48d96a8084
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-05-18 16:35:41 +0200

    Add generateKey function

--- lc4.js
+++ lc4.js
@@ -2,3 +2,23 @@ import { shuffle } from "./helpers.js";
 
 const ALPHABET = "#_23456789abcdefghijklmnopqrstuvwxyz";
 const GRIDSIZE = 6;
+
+function generateKey(keyword = false) {
+    if (keyword) {
+        if (![...keyword].every(char => ALPHABET.indexOf(char) > -1))
+            throw new Error(
+                "Keyword for key generation contains invalid characters!\n" +
+                    "You may only use following characters: " +
+                    ALPHABET
+            );
+    }
+
+    // Shuffle alphabet without letters already in keyword
+    let key = shuffle(
+        [...ALPHABET].filter(char =>
+            keyword ? keyword.indexOf(char) > -1 : true
+        )
+    );
+
+    return (keyword ? keyword : "") + key;
+}
