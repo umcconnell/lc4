c270bf5d58dbe9daa4a80b4860e18b305394f9a8
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-05-18 16:24:07 +0200

    Add fisher-yates shuffle to shuffle

---
+++ helpers.js
@@ -0,0 +1,22 @@
+function shuffle(arr) {
+    // Copy the array
+    arr = arr.slice();
+
+    // Fisher-Yates Shuffle
+    // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    let l = arr.length,
+        temp,
+        i;
+
+    while (l) {
+        i = Math.floor(Math.random() * l--);
+
+        temp = arr[l];
+        arr[l] = arr[i];
+        arr[i] = temp;
+    }
+
+    return arr;
+}
+
+export { shuffle };
