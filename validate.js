5ae7d1912bf91c71f47da115d69091a3f8d842cc
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-05-18 18:35:32 +0200

    Add validate.js file
    Move function validation to here.

---
+++ validate.js
@@ -0,0 +1,35 @@
+import { validLC4 } from "./helpers.js";
+import { ALPHABET } from "./config.js";
+
+function validateEncryptSettings(settings) {
+    if (!validLC4(settings.key)) {
+        throw new Error(
+            "Keyword for key generation contains invalid characters!\n" +
+                "You may only use following characters: " +
+                ALPHABET
+        );
+    } else if (
+        settings.nonce &&
+        (!validLC4(settings.nonce) || settings.nonce.length < 6)
+    ) {
+        throw new Error(
+            "Invalid nonce!\n" +
+                "Nonce may only contain following characters: " +
+                ALPHABET +
+                " and must be at least 6 characters long."
+        );
+    } else if (
+        settings.signature &&
+        (!validLC4(settings.signature) || settings.signature.length < 10)
+    ) {
+        throw new Error(
+            "Invalid signature!\n" +
+                "Signature may only contain following characters: " +
+                ALPHABET +
+                " and must be at least 10 characters long."
+        );
+    } else if (!settings.msg)
+        throw new Error("You must specify a message to encrypt");
+}
+
+export { validateEncryptSettings };
