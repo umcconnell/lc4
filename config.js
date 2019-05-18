df31a067add65c5184c9068ed5e87c13f913398e
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-05-18 18:26:11 +0200

    Add config file with default values

---
+++ config.js
@@ -0,0 +1,5 @@
+const ALPHABET = "#_23456789abcdefghijklmnopqrstuvwxyz";
+const GRIDSIZE = 6;
+const DEFAULT_SETTINGS = { signature: null, headerData: null, nonce: null };
+
+export { ALPHABET, GRIDSIZE, DEFAULT_SETTINGS };
