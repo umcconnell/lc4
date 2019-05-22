8c2ee12bcca500c847d1ed1c822ce5895ef8e116
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-05-22 16:07:30 +0200

    Throw TypeError on validation error

--- src/validate.js
+++ src/validate.js
@@ -3,17 +3,17 @@ import { ALPHABET } from "./config.js";
 
 export function validateSettings(settings) {
     if (!settings.message) {
-        throw new Error("You must specify a message to encrypt");
+        throw new TypeError("You must specify a message to encrypt");
     } else if (!settings.key) {
-        throw new Error(
+        throw new TypeError(
             "You must specify a (valid) key!\n" +
                 "You may only use following characters: " +
                 ALPHABET
         );
     } else if (settings.key.length !== ALPHABET.length) {
-        throw new Error("Key is too short");
+        throw new TypeError("Key is too short");
     } else if (!validLC4([...settings.key])) {
-        throw new Error(
+        throw new TypeError(
             "Keyword for key generation contains invalid characters!\n" +
                 "You may only use following characters: " +
                 ALPHABET
@@ -22,7 +22,7 @@ export function validateSettings(settings) {
         settings.nonce &&
         (!validLC4([...settings.nonce]) || settings.nonce.length < 6)
     ) {
-        throw new Error(
+        throw new TypeError(
             "Invalid nonce!\n" +
                 "Nonce may only contain following characters: " +
                 ALPHABET +
@@ -32,7 +32,7 @@ export function validateSettings(settings) {
         settings.signature &&
         (!validLC4([...settings.signature]) || settings.signature.length < 10)
     ) {
-        throw new Error(
+        throw new TypeError(
             "Invalid signature!\n" +
                 "Signature may only contain following characters: " +
                 ALPHABET +
