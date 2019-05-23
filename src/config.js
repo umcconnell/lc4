241baa97b1e5309b416760b71d2bf22b2228aab9
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-05-23 20:52:18 +0200

    Document config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
+/** @module lc4/config */
+
+/** LC4 alphabet */
 export const ALPHABET = "#_23456789abcdefghijklmnopqrstuvwxyz";
+/** LC4 state grid size */
 export const GRIDSIZE = 6;
+/** Default LC4 encryption/decryption settings */
 export const DEFAULT_SETTINGS = {
     signature: null,
     headerData: null,
