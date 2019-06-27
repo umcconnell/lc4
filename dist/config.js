d1ce3c2a74ca3915a1ac0e5ea689058327aaa6a9
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-06-27 17:59:35 +0200

    Add minified build for the browser

---
+++ dist/config.js
@@ -0,0 +1,2 @@
+export const ALPHABET="#_23456789abcdefghijklmnopqrstuvwxyz";export const GRIDSIZE=6;export const DEFAULT_SETTINGS={signature:null,headerData:null,nonce:null,verbose:!1};
+//# sourceMappingURL=config.js.map
