15f7773257553d55507d46e04c0976e4aed0e6d1
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-06-29 13:57:56 +0200

    Respect ls47 mode in validation

--- src/validate.js
+++ src/validate.js
@@ -1,6 +1,6 @@
 /** @module lc4/validate */
 import { validString } from "./helpers.js";
-import { ALPHABET } from "./config.js";
+import { ALPHABET, ALPHABET_LS47 } from "./config.js";
 
 /**
  * Validate encryption/decryption LC4 settings
@@ -18,49 +18,50 @@ import { ALPHABET } from "./config.js";
 export function validateSettings(settings) {
     if (!settings.message) {
         throw new TypeError("You must specify a message to encrypt");
-    } else if (!settings.key) {
-        throw new TypeError(
-            "You must specify a (valid) key!\n" +
-                "You may only use following characters: " +
-                ALPHABET
-        );
-    } else if (settings.key.length !== ALPHABET.length) {
-        throw new TypeError("Key is too short");
     } else if (
         settings.mode &&
-        !(
-            settings.mode.toLowerCase() === "lc4" ||
-            settings.mode.toLowerCase() === "ls47"
-        )
+        !(settings.mode === "lc4" || settings.mode === "ls47")
     ) {
         throw new TypeError(
             "Invalid mode!\n" + "Mode may be either 'lc4' or 'ls47'."
         );
-    } else if (!validString([...settings.key])) {
+    } else if (!settings.key) {
+        throw new TypeError(
+            "You must specify a (valid) key!\n" +
+                "You may only use following characters: " +
+                (settings.mode === "lc4" ? ALPHABET : ALPHABET_LS47)
+        );
+    } else if (
+        settings.key.length !==
+        (settings.mode === "lc4" ? ALPHABET : ALPHABET_LS47).length
+    ) {
+        throw new TypeError("Key is too short");
+    } else if (!validString([...settings.key], settings.mode)) {
         throw new TypeError(
             "Keyword for key generation contains invalid characters!\n" +
                 "You may only use following characters: " +
-                ALPHABET
+                (settings.mode === "lc4" ? ALPHABET : ALPHABET_LS47)
         );
     } else if (
         settings.nonce &&
-        (!validString([...settings.nonce]) || settings.nonce.length < 6)
+        (!validString([...settings.nonce], settings.mode) ||
+            settings.nonce.length < 6)
     ) {
         throw new TypeError(
             "Invalid nonce!\n" +
                 "Nonce may only contain following characters: " +
-                ALPHABET +
+                (settings.mode === "lc4" ? ALPHABET : ALPHABET_LS47) +
                 " and must be at least 6 characters long."
         );
     } else if (
         settings.signature &&
-        (!validString([...settings.signature]) ||
+        (!validString([...settings.signature], settings.mode) ||
             settings.signature.length < 10)
     ) {
         throw new TypeError(
             "Invalid signature!\n" +
                 "Signature may only contain following characters: " +
-                ALPHABET +
+                (settings.mode === "lc4" ? ALPHABET : ALPHABET_LS47) +
                 " and must be at least 10 characters long."
         );
     }
