7adeae2f90a46c31ddd6808a381700c75f416c72
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-06-29 13:46:00 +0200

    Add LS47 config (alphabet, gridsize)

--- src/config.js
+++ src/config.js
@@ -2,9 +2,14 @@
 
 /** LC4 alphabet */
 export const ALPHABET = "#_23456789abcdefghijklmnopqrstuvwxyz";
+/** LS47 alphabet */
+export const ALPHABET_LS47 =
+    "_abcdefghijklmnopqrstuvwxyz.0123456789,-+*/:?!'()";
 /** LC4 state grid size */
 export const GRIDSIZE = 6;
-/** Default LC4 encryption/decryption settings */
+/** LS47 state grid size */
+export const GRIDSIZE_LS47 = 7;
+/** Default LC4/LS47 encryption/decryption settings */
 export const DEFAULT_SETTINGS = {
     mode: "lc4",
     signature: null,
