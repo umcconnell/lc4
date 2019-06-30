73f7e3f6068d4b32cb3a15e39c2b58757ff42cca
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-06-30 16:21:32 +0200

    Improve encryptMsg documentation in lc4.js

--- src/lc4.js
+++ src/lc4.js
@@ -123,6 +123,8 @@ export function initState(key, mode = "lc4") {
  * @param {Object} env.marker marker object representing active element
  * @param {Number} env.marker.i row of the marker in the state
  * @param {Number} env.marker.j column of the marker in the state
+ * @param {String} env.mode encryption algorithm. Can be either
+ * "lc4" or "ls47"
  * @param {String} msg cleartext message
  * @param {Boolean} [verbose=false] boolean indicating wether verbose mode
  * should be used (will print out intermediate steps)
