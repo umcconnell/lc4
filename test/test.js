aa6a55d370bfd5aa5f386cf286db766405a54cdf
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2019-07-07 20:35:39 +0200

    Update key tests
    Previously invalid keys are now allowed as passwords. Now tests
    check whether TypeError is thrown when duplicate characters are
    passed.

--- test/test.js
+++ test/test.js
@@ -181,14 +181,40 @@ describe("Input validation", () => {
     describe("invalid key passed", () => {
         it("should throw at encryption", () => {
             expect(
-                encrypt.bind(null, { message: "hi", key: "invalid" })
+                encrypt.bind(null, {
+                    message: "hi",
+                    // Duplicate "r"
+                    key: "rruoq8_slktcf7h9xbj#vg24m5ie6ad3ynpw"
+                })
             ).to.throw(TypeError);
+
+            expect(
+                encrypt.bind(null, {
+                    message: "hi",
+                    // Duplicate "0"
+                    key: "00cdsgj:74('bk6m!nxlq23-*wae.v89p?/_5zfhr1uo+),ty",
+                    mode: "ls47"
+                })
+            );
         });
 
         it("should throw at decryption", () => {
             expect(
-                decrypt.bind(null, { message: "hi", key: "invalid" })
+                decrypt.bind(null, {
+                    message: "hi",
+                    // Duplicate "l"
+                    key: "ll6mxhzsd2y3bqrij7onkgav#w9pect8f4_u"
+                })
             ).to.throw(TypeError);
+
+            expect(
+                decrypt.bind(null, {
+                    message: "hi",
+                    // Duplicate "4"
+                    key: "440e38)1xvn6bk2,-w'rt(dgl/mfq_!a+husyz795?j:ipc.o",
+                    mode: "ls47"
+                })
+            );
         });
     });
 });
