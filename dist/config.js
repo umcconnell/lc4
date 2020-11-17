1d4900668a2e175a4b025a5e5fd3686ca7e4c5c8
AGPLCZ <62847503+AGPLCZ@users.noreply.github.com>
2020-11-17 07:40:05 +0000

    Update config.js

--- dist/config.js
+++ dist/config.js
@@ -1,2 +1,2 @@
-export const ALPHABET="#_23456789abcdefghijklmnopqrstuvwxyz";export const ALPHABET_LS47="_abcdefghijklmnopqrstuvwxyz.0123456789,-+*/:?!'()";export const GRIDSIZE=6;export const GRIDSIZE_LS47=7;export const DEFAULT_SETTINGS={mode:"lc4",signature:null,headerData:null,nonce:null,verbose:!1};
+export const ALPHABET=" 123456789abcdefghijklmnopqrstuvwxyz";export const ALPHABET_LS47=" abcdefghijklmnopqrstuvwxyz.0123456789,-+*/:?!'()";export const GRIDSIZE=6;export const GRIDSIZE_LS47=7;export const DEFAULT_SETTINGS={mode:"lc4",signature:null,headerData:null,nonce:null,verbose:!1};
 //# sourceMappingURL=config.js.map
